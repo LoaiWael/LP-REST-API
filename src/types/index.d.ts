0770b6a88d856a366be227fd4bee7c5aa06129c4
Loai Wael <loaiwael.dev@gmail.com>
2026-05-23 22:42:49 +0300

    UPD types by converting to .d.ts definition file

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+import { components } from "../schemas/index.js"
+
+export type Skill = components['schemas']['Skill'];
+
+export type Contributor = components['schemas']['Contributor']
+
+export type Work = components['schemas']['Work']
+
+export type Experience = components['schemas']['Experience']
+
+export type ErrorDto = components['schemas']['Error']
