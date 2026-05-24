36b0b2fe1f8d9d79e34fbe607aa1b774c0dceb65
Loai Wael <loaiwael.dev@gmail.com>
2026-05-24 20:22:37 +0300

    DEL removed ajv validations and deprecated schemas

--- src/validations/index.ts
+++
@@ -1,4 +0,0 @@
-export { validateSkillPost, validateSkillPut } from "./skill.schema.js";
-export { validateContributorPost, validateContributorPut } from "./contributor.schema.js";
-export { validateWorkPost, validateWorkPut } from "./work.schema.js";
-export { validateExperiencePost, validateExperiencePut } from "./experience.schema.js";
